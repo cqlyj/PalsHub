ed4a74a5068a73b9cc94887ac6935b72eec3f168
cqlyj <luoyingjie0721@gmail.com>
2024-04-30 21:20:23 +0800

    Refactor ChatPage component to include ChatSideBar and ChatChannel components

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,23 +1,33 @@
 "use client";
 
-import { UserButton, useUser } from "@clerk/nextjs";
-import { StreamChat } from "stream-chat";
-import {
-  Channel,
-  ChannelHeader,
-  ChannelList,
-  Chat,
-  LoadingIndicator,
-  MessageInput,
-  MessageList,
-  Thread,
-  Window,
-} from "stream-chat-react";
+import { useUser } from "@clerk/nextjs";
+import { Chat, LoadingIndicator } from "stream-chat-react";
 import useInitializeChatClient from "./useInitializeChatClient";
+import MenuBar from "./MenuBar";
+import ChatSideBar from "./ChatSideBar";
+import ChatChannel from "./ChatChannel";
+import { useCallback, useEffect, useState } from "react";
+import { Menu, X } from "lucide-react";
+import useWindowSize from "../../hooks/useWindowSize";
+import { mdBreakpoint } from "@/utils/tailwind";
 
 const ChatPage = () => {
   const chatClient = useInitializeChatClient();
   const { user } = useUser();
+
+  const [chatSidebarOpen, setChatSidebarOpen] = useState(false);
+  const windowSize = useWindowSize();
+  const isLargeScreen = windowSize.width >= mdBreakpoint;
+
+  useEffect(() => {
+    if (windowSize.width >= mdBreakpoint) {
+      setChatSidebarOpen(false);
+    }
+  }, [windowSize.width]);
+
+  const handleSidebarOnClose = useCallback(() => {
+    setChatSidebarOpen(false);
+  }, []);
 
   if (!user || !chatClient) {
     return (
@@ -28,31 +38,33 @@ const ChatPage = () => {
   }
 
   return (
-    <div className="h-screen">
-      <Chat client={chatClient}>
-        <div className="flex h-full flex-row">
-          <div className="w-full max-w-[360px]">
-            <ChannelList
-              filters={{
-                type: "messaging",
-                members: { $in: [user.id] },
-              }}
-              sort={{ last_message_at: -1 }}
-              options={{ state: true, presence: true, limit: 10 }}
-            ></ChannelList>
+    <div className="h-screen bg-gray-100 xl:px-20 xl:py-8">
+      <div className="m-auto flex h-full min-w-[350px] max-w-[1600px] flex-col shadow-sm">
+        <Chat client={chatClient}>
+          <div className="flex justify-center border-b border-b-[#DBDDE1] p-3 md:hidden">
+            <button onClick={() => setChatSidebarOpen(!chatSidebarOpen)}>
+              {!chatSidebarOpen ? (
+                <span className="flex items-center gap-1">
+                  <Menu>Menu</Menu>
+                </span>
+              ) : (
+                <X></X>
+              )}
+            </button>
           </div>
-          <div className="h-full w-full">
-            <Channel>
-              <Window>
-                <ChannelHeader></ChannelHeader>
-                <MessageList></MessageList>
-                <MessageInput></MessageInput>
-              </Window>
-              <Thread></Thread>
-            </Channel>
+          <div className="flex h-full flex-row overflow-y-auto">
+            <ChatSideBar
+              user={user}
+              show={isLargeScreen || chatSidebarOpen}
+              onClose={handleSidebarOnClose}
+            ></ChatSideBar>
+            <ChatChannel
+              show={isLargeScreen || !chatSidebarOpen}
+              hideChannelOnThread={!isLargeScreen}
+            ></ChatChannel>
           </div>
-        </div>
-      </Chat>
+        </Chat>
+      </div>
     </div>
   );
 };
