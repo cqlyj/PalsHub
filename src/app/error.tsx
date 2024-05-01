078e75a280db939590e716bf928ff05fbca08784
cqlyj <luoyingjie0721@gmail.com>
2024-05-02 03:16:30 +0800

    Add ErrorPage component

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const ErrorPage = () => {
+  return (
+    <div className="space-y-3 text-center">
+      <h1 className="text-2xl font-bold">Error</h1>
+      <p>Sorry, something went wrong. Try again later.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
