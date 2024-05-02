f065227c32e901f0b4158db1352a95cb0e3b8425
cqlyj <luoyingjie0721@gmail.com>
2024-05-02 18:40:52 +0800

    Refactor MeetingPage component and add generateMetadata function in page.tsx

--- src/app/CreateMeetingPage.tsx
+++ src/app/CreateMeetingPage.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import Button from "@/components/Button";
 import { useUser } from "@clerk/nextjs";
-import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
-import { Loader2, Tractor } from "lucide-react";
+import {
+  Call,
+  MemberRequest,
+  useStreamVideoClient,
+} from "@stream-io/video-react-sdk";
+import { Copy, Loader2 } from "lucide-react";
+import Link from "next/link";
 import { useState } from "react";
+import { getUserIds } from "./actions";
 
-const CreateMeetingPage = () => {
+export default function CreateMeetingPage() {
   const [descriptionInput, setDescriptionInput] = useState("");
   const [startTimeInput, setStartTimeInput] = useState("");
   const [participantsInput, setParticipantsInput] = useState("");
@@ -23,50 +30,69 @@ const CreateMeetingPage = () => {
 
     try {
       const id = crypto.randomUUID();
-      const call = client.call("default", id);
+
+      const callType = participantsInput ? "private-meeting" : "default";
+
+      const call = client.call(callType, id);
+
+      const memberEmails = participantsInput
+        .split(",")
+        .map((email) => email.trim());
+
+      const memberIds = await getUserIds(memberEmails);
+
+      const members: MemberRequest[] = memberIds
+        .map((id) => ({ user_id: id, role: "call_member" }))
+        .concat({ user_id: user.id, role: "call_member" })
+        .filter(
+          (v, i, a) => a.findIndex((v2) => v2.user_id === v.user_id) === i,
+        );
+
+      const starts_at = new Date(startTimeInput || Date.now()).toISOString();
+
       await call.getOrCreate({
         data: {
+          starts_at,
+          members,
           custom: { description: descriptionInput },
         },
       });
+
       setCall(call);
     } catch (error) {
       console.error(error);
-      alert("Something went wrong. Please try again.");
+      alert("Something went wrong. Please try again later.");
     }
   }
 
   if (!client || !user) {
-    return <Loader2 className="mx-auto animate-spin"></Loader2>;
+    return <Loader2 className="mx-auto animate-spin" />;
   }
 
   return (
     <div className="flex flex-col items-center space-y-6">
       <h1 className="text-center text-2xl font-bold">
-        Welcome {user.username}
+        Welcome {user.username}!
       </h1>
       <div className="mx-auto w-80 space-y-6 rounded-md bg-slate-100 p-5">
         <h2 className="text-xl font-bold">Create a new meeting</h2>
         <DescriptionInput
           value={descriptionInput}
           onChange={setDescriptionInput}
-        ></DescriptionInput>
-        <StartTimeInput
-          value={startTimeInput}
-          onChange={setStartTimeInput}
-        ></StartTimeInput>
+        />
+        <StartTimeInput value={startTimeInput} onChange={setStartTimeInput} />
         <ParticipantsInput
           value={participantsInput}
           onChange={setParticipantsInput}
-        ></ParticipantsInput>
-        <button className="w-full" onClick={createMeeting}>
+        />
+        <Button onClick={createMeeting} className="w-full">
           Create meeting
-        </button>
+        </Button>
       </div>
-      {call && <MeetingLink call={call}></MeetingLink>}
+      {call && <MeetingLink call={call} />}
     </div>
   );
-};
+}
 
 interface DescriptionInputProps {
   value: string;
@@ -75,6 +101,7 @@ interface DescriptionInputProps {
 
 function DescriptionInput({ value, onChange }: DescriptionInputProps) {
   const [active, setActive] = useState(false);
+
   return (
     <div className="space-y-2">
       <div className="font-medium">Meeting info:</div>
@@ -94,10 +121,10 @@ function DescriptionInput({ value, onChange }: DescriptionInputProps) {
           <span className="font-medium">Description</span>
           <textarea
             value={value}
-            onChange={(e) => e.target.value}
+            onChange={(e) => onChange(e.target.value)}
             maxLength={500}
             className="w-full rounded-md border border-gray-300 p-2"
-          ></textarea>
+          />
         </label>
       )}
     </div>
@@ -113,9 +140,9 @@ function StartTimeInput({ value, onChange }: StartTimeInputProps) {
   const [active, setActive] = useState(false);
 
   const dateTimeLocalNow = new Date(
-    new Date().getTime() - new Date().getTimezoneOffset() * 60000,
+    new Date().getTime() - new Date().getTimezoneOffset() * 60_000,
   )
-    .toString()
+    .toISOString()
     .slice(0, 16);
 
   return (
@@ -130,7 +157,7 @@ function StartTimeInput({ value, onChange }: StartTimeInputProps) {
             onChange("");
           }}
         />
-        Start now
+        Start meeting immediately
       </label>
       <label className="flex items-center gap-1.5">
         <input
@@ -145,13 +172,11 @@ function StartTimeInput({ value, onChange }: StartTimeInputProps) {
       </label>
       {active && (
         <label className="block space-y-1">
-          <span className="font-medium"></span>
+          <span className="font-medium">Start time</span>
           <input
             type="datetime-local"
             value={value}
-            onChange={(e) => {
-              onChange(e.target.value);
-            }}
+            onChange={(e) => onChange(e.target.value)}
             min={dateTimeLocalNow}
             className="w-full rounded-md border border-gray-300 p-2"
           />
@@ -184,13 +209,7 @@ function ParticipantsInput({ value, onChange }: ParticipantsInputProps) {
         Everyone with link can join
       </label>
       <label className="flex items-center gap-1.5">
-        <input
-          type="radio"
-          checked={active}
-          onChange={() => {
-            setActive(true);
-          }}
-        />
+        <input type="radio" checked={active} onChange={() => setActive(true)} />
         Private meeting
       </label>
       {active && (
@@ -198,10 +217,10 @@ function ParticipantsInput({ value, onChange }: ParticipantsInputProps) {
           <span className="font-medium">Participant emails</span>
           <textarea
             value={value}
-            onChange={(e) => e.target.value}
-            placeholder="Enter participant emails separated by commas"
+            onChange={(e) => onChange(e.target.value)}
+            placeholder="Enter participant email addresses separated by commas"
             className="w-full rounded-md border border-gray-300 p-2"
-          ></textarea>
+          />
         </label>
       )}
     </div>
@@ -215,7 +234,61 @@ interface MeetingLinkProps {
 function MeetingLink({ call }: MeetingLinkProps) {
   const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${call.id}`;
 
-  return <div className="text-center">{meetingLink}</div>;
+  return (
+    <div className="flex flex-col items-center gap-3 text-center">
+      <div className="flex items-center gap-3">
+        <span>
+          Invitation link:{" "}
+          <Link href={meetingLink} className="font-medium">
+            {meetingLink}
+          </Link>
+        </span>
+        <button
+          title="Copy invitation link"
+          onClick={() => {
+            navigator.clipboard.writeText(meetingLink);
+            alert("Copied to clipboard");
+          }}
+        >
+          <Copy />
+        </button>
+      </div>
+      <a
+        href={getMailToLink(
+          meetingLink,
+          call.state.startsAt,
+          call.state.custom.description,
+        )}
+        target="_blank"
+        className="text-blue-500 hover:underline"
+      >
+        Send email invitation
+      </a>
+    </div>
+  );
 }
 
-export default CreateMeetingPage;
+function getMailToLink(
+  meetingLink: string,
+  startsAt?: Date,
+  description?: string,
+) {
+  const startDateFormatted = startsAt
+    ? startsAt.toLocaleString("en-US", {
+        dateStyle: "full",
+        timeStyle: "short",
+      })
+    : undefined;
+
+  const subject =
+    "Join my meeting" + (startDateFormatted ? ` at ${startDateFormatted}` : "");
+
+  const body =
+    `Join my meeting at ${meetingLink}.` +
+    (startDateFormatted
+      ? `\n\nThe meeting starts at ${startDateFormatted}.`
+      : "") +
+    (description ? `\n\nDescription: ${description}` : "");
+
+  return `mailto:?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+}
