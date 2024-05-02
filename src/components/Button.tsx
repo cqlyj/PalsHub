d1e998b6567160efdc3af141fbe6e3ea87da75bf
cqlyj <luoyingjie0721@gmail.com>
2024-05-02 18:39:28 +0800

    Add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+const Button = ({
+  className,
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      className={cn(
+        "flex items-center justify-center gap-2 rounded-full bg-blue-500 px-3 py-2 font-semibold text-white transition-colors hover:bg-blue-600 active:bg-blue-600 disabled:bg-gray-200",
+        className,
+      )}
+      {...props}
+    ></button>
+  );
+};
+
+export default Button;
