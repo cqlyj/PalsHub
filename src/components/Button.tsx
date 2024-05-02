9cddb0c0b21b484d1ca7de9cbf627abec1e32c9e
cqlyj <luoyingjie0721@gmail.com>
2024-05-03 01:25:11 +0800

    Refactor Button component to use buttonClassName constant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,14 +6,11 @@ const Button = ({
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button
-      className={cn(
-        "flex items-center justify-center gap-2 rounded-full bg-blue-500 px-3 py-2 font-semibold text-white transition-colors hover:bg-blue-600 active:bg-blue-600 disabled:bg-gray-200",
-        className,
-      )}
-      {...props}
-    ></button>
+    <button className={cn(buttonClassName, className)} {...props}></button>
   );
 };
 
 export default Button;
+
+export const buttonClassName =
+  "flex items-center justify-center gap-2 rounded-full bg-blue-500 px-3 py-2 font-semibold text-white transition-colors hover:bg-blue-600 active:bg-blue-600 disabled:bg-gray-200";
