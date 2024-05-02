59473e20ce5ef4681adef0d3425d38ad25f0fe13
cqlyj <luoyingjie0721@gmail.com>
2024-05-02 17:38:48 +0800

    Update CSS styles and import video-react-sdk CSS in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ClientProvider from "./ClientProvider";
