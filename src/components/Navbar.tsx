f4f506f43581252800adb8b8619d097c5be18dbc
cqlyj <luoyingjie0721@gmail.com>
2024-05-06 20:27:48 +0800

    add logo to the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
+import logo from "../../public/palshub_logo.png";
 
 const Navbar = () => {
   return (
     <header className="shadow">
       <div className="mx-auto flex h-14 max-w-5xl items-center justify-between p-3 font-medium">
-        <Link href="/">New meeting</Link>
+        <div className="flex min-w-[180px] flex-row items-center justify-between gap-2 p-3">
+          <Image src={logo} alt="logo" width={40} height={40}></Image>
+          <Link href="/">New meeting</Link>
+        </div>
         <SignedIn>
           <div className="flex items-center gap-5">
             <Link href="/meetings">Meetings</Link>
